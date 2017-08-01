b04c7c8e34a53c046fba87f693d3273595911265
Sean O'Hollaren <Sean.OHollaren2@nike.com>
2017-08-01 13:30:47 -0700

    Add check for missing endpoint

--- example.js
+++ example.js
@@ -7,6 +7,12 @@ const imageLocation = './test.jpg';
 // the APIG endpoint which triggers the rekognition lambda
 const endpoint = '';
 
+// make sure the endpoint was specified
+if (!endpoint || endpoint === '') {
+  console.log('Error: Update example.js to specify the url Serverless created when deploying your lambda');
+  return;
+}
+
 // pull base64 representation of image from file system
 const image = fs.readFile(imageLocation, 'base64', (err, data) => {
 
